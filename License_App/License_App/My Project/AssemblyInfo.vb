c190ba7fb5019859d60034a2159723b7b9c87acd
Prasop Arunprai <arunprai@gmail.com>
2015-05-15 14:37:52 +0700

    Create Project

--- License_App/License_App/My Project/AssemblyInfo.vb
+++ License_App/License_App/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("WindowsApplication1")> 
+<Assembly: AssemblyTitle("License_App")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("WindowsApplication1")> 
+<Assembly: AssemblyProduct("License_App")> 
 <Assembly: AssemblyCopyright("Copyright ©  2015")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("6a081c98-7917-4fa4-b86f-d310fa2b4db6")> 
+<Assembly: Guid("5fe4238c-6721-44d7-94ea-497f0fdceacf")> 
 
 ' Version information for an assembly consists of the following four values:
 '
